967089484ce61c7644c660b66e1595c6d1c1d528
Paulo Jabardo <pjabardo@gmail.com>
2021-09-17 17:22:23 -0300

    Implemented the Probe1d and Probe3d stuff
    Including velocity decomposition for 3d probes. No tests were implemented.

--- src/Hotwire.jl
+++ src/Hotwire.jl
@@ -7,6 +7,11 @@ export CCASensor, current
 export tempcorr
 export calibr_curve, CalibrCurve, ExtrapolateFit
 
+export AbstractProbe, AbstractProbe1d, AbstractProbe2d, AbstractProbe3d
+export HWCable, HWSupport, HWBridge
+export Probe1d, Probe2d, Probe3d
+
+
 include("resistor.jl")
 include("wire.jl")
 include("tempcorr.jl")
--- src/probes.jl
+++ src/probes.jl
@@ -1,4 +1,6 @@
 
+using StaticArrays
+
 abstract type AbstractProbe end
 
 abstract type AbstractProbe1d <: AbstractProbe end
@@ -40,7 +42,7 @@ struct HWCable
     "Length of the cable"
     L::Float64
     "Connector types (left and right)"
-    conn::{String,String}
+    conn::Tuple{String,String}
 end
 
 """
@@ -94,16 +96,16 @@ HWBridge(;model="Streamline", tag="", id=1, offset=0.0, gain=1.0,
                       coolinterv)
 
 mutable struct Probe1d{Anem<:AbstractThermalAnemometer,Calibr} <: AbstractProbe1d
+    "Anemometer sensor information"
+    sensor::Anem
+    "Calibration data"
+    cal::Calibr
     "Probe model"
     model::String
     "Probe tag"
     tag::String
     "Probe leads resistance"
     RL::Float64
-    "Anemometer sensor information"
-    sensor::Anem
-    "Calibration data"
-    cal::Calibr
     "Hardware information"
     bridge::HWBridge
     "Probe support"
@@ -112,16 +114,36 @@ mutable struct Probe1d{Anem<:AbstractThermalAnemometer,Calibr} <: AbstractProbe1
     cable::HWCable
 end
 
+"""
+    `Probe1d(sensor, cal; model="model", tag="HW 1d", RL=0.5, 
+             bridge=HWBridge(), support=HWSupport(), cable=HWCable())`
+
+Creates a 1d probe. This completely characterizes a 1d probe. It includes
+information on calibration, bridge configuration, output filter, etc.
+
+# Arguments:
+
+ - `sensor`: an `AbstractThermalAnemometer` object
+ - `cal`: `CalibrCurve` object
+ - `model`: Probe model
+ - `tag`: Probe id tag
+ - `RL`: Lead resistance
+ - `bridge`: Bridge configuration
+ - `support`: `HWSupport` object specifying the probe support
+ - `cable`: `HWCable` object with information on cable
+
+"""
+function Probe1d(sensor, cal; model="", tag="", RL=0.5,
+                 bridge=HWBridge(id=1),
+                 support=HWSupport(), cable=HWCable())
+    return Probe1d(sensor, cal, model, tag, RL, bridge, support, cable)
+end
+
+                                                        
 (anem::Probe1d)(E, temp) = anem.cal(anem.sensor, E, temp)
 (anem::Probe1d)(E) = anem.cal(anem.sensor, E, anem.cal.T0)
 
 mutable struct Probe2d{Anem<:AbstractThermalAnemometer,Calibr} <: AbstractProbe2d
-    "Probe model"
-    model::String
-    "Probe tag"
-    tag::String
-    "Probe leads resistance"
-    RL::NTuple{2,Float64}
     "Sensor information"
     sensor::NTuple{2,Anem}
     "Sensor calibration"
@@ -129,9 +151,15 @@ mutable struct Probe2d{Anem<:AbstractThermalAnemometer,Calibr} <: AbstractProbe2
     "k² directional calibration for each sensor"
     k²::NTuple{2,Float64}
     "h² directional calibration for each sensor"
-    h²::NTuple{2,FLoat64}
+    h²::NTuple{2,Float64}
     "Matrix with the cosine of each wire with respect to x, y and z axes"
     cosϕ::Matrix{Float64}
+    "Probe model"
+    model::String
+    "Probe tag"
+    tag::String
+    "Probe leads resistance"
+    RL::NTuple{2,Float64}
     "Hardware information"
     bridge::NTuple{2,HWBridge}
     "Probe support"
@@ -141,14 +169,7 @@ mutable struct Probe2d{Anem<:AbstractThermalAnemometer,Calibr} <: AbstractProbe2
     
 end
 
-
 mutable struct Probe3d{Anem<:AbstractThermalAnemometer,Calibr} <: AbstractProbe3d
-    "Probe model"
-    model::String
-    "Probe tag"
-    tag::String
-    "Probe leads resistance"
-    RL::NTuple{3,Float64}
     "Sensor information"
     sensor::NTuple{3,Anem}
     "Sensor calibration"
@@ -156,19 +177,122 @@ mutable struct Probe3d{Anem<:AbstractThermalAnemometer,Calibr} <: AbstractProbe3
     "k² directional calibration for each sensor"
     k²::NTuple{3,Float64}
     "h² directional calibration for each sensor"
-    h²::NTuple{3,FLoat64}
-    "Matrix with the cosine of each wire with respect to x, y and z axes"
-    cosϕ::Matrix{Float64}
+    h²::NTuple{3,Float64}
+    "Cosine of wire direction with respect to axes x, y and z"
+    cosϕ::SMatrix{3,3,Float64,9}
+    "Probe model"
+    model::String
+    "Probe tag"
+    tag::String
+    "Probe leads resistance"
+    RL::NTuple{3,Float64}
+    "Factor to calculate effective velocity from calibration velocity"
+    c2e::SVector{3,Float64}
+    "Matrix to calculate velocity in wire coordinates from effective vel."
+    A::SMatrix{3,3,Float64,9}
     "Hardware information"
     bridge::NTuple{3,HWBridge}
     "Probe support"
     support::HWSupport
     "Connecting cable"
     cable::NTuple{3,HWCable}
+end
+
+"""
+    `Probe3d(sensor, cal, k², h²; ϕ=ϕ, model="model", tag="HW 1d", RL=0.5, 
+             bridge=br, support=su, cable=cc)`
+
+Creates a 2d probe. This completely characterizes a 1d probe. It includes
+information on calibration, directional calibration, bridge configuration,
+ output filter, etc.
+The effective velocity acting on a wire is given by 
+``
+(Uᵉ)² = U₁² + h²⋅U₂² + k²⋅U₃²
+``
+In this equation, Uᵉ is the effective velocity, U₁ is the velocity normal 
+to the wire and in the direction of the probe body, U₂ is the velocity 
+normal to the flow but also normal to the body and U₃ is the velocity 
+along the wire.
+
+The function assumes that the calibration was carried out with flow along
+the x axis. 
+
+# Arguments:
+
+ - `sensor`: a tuple of `AbstractThermalAnemometer` objects (3)
+ - `cal`: `CalibrCurve` object
+ - `model`: Probe model
+ - `tag`: Probe id tag
+ - `RL`: Lead resistance
+ - `bridge`: Bridge configuration
+ - `support`: `HWSupport` object specifying the probe support
+ - `cable`: `HWCable` object with information on cable
+
+"""
+function Probe3d(sensor, cal, k², h²; ϕ=[125.264 45 114.094;
+                                         125.264 135 114.094;
+                                         125.264 90 35.264],
+                 model="55R91", tag="", RL=0.5,
+                 bridge=(HWBridge(), HWBridge(), HWBridge()),
+                 support=HWSupport(),
+                 cable=(HWCable(), HWCable(), HWCable()))
+
+    # Calculate the cosine of the angles
+    cosϕ = SMatrix{3,3,Float64}(cosd.(ϕ))
+    c2e = SVector{3,Float64}( (1.0 + k²[1] + h²[1])*cosϕ[1,1]^2,
+                              (1.0 + k²[2] + h²[2])*cosϕ[2,1]^2,
+                              (1.0 + k²[3] + h²[3])*cosϕ[3,1]^2 )
+    A = SMatrix{3,3,Float64}([k²[1] 1      h²[1];
+                              h²[2] k²[2]  1;
+                              1     h²[3]  k²[3]])
+    return Probe3d((sensor[1], sensor[2], sensor[3]),
+                   (cal[1], cal[2], cal[3]), (k²[1], k²[2], k²[3]),
+                   (h²[1], h²[2], h²[3]), cosϕ, model, tag, RL,
+                   c2e, inv(A), (bridge[1],bridge[2],bridge[3]),
+                   support, (cable[1],cable[2],cable[3]))
+end
+
+function (anem::Probe3d)(E₁, E₂, E₃, T)
+
+    # Calibration curve and temperature correction for
+    # each sensor and calculation of effective velocity for each wire
+    Uᵉ = SVector(anem.cal[1](anem.sensor[1], E₁, T)^2 * c2e[1], 
+                 anem.cal[1](anem.sensor[1], E₂, T)^2 * c2e[2], 
+                 anem.cal[1](anem.sensor[1], E₃, T)^2 * c2e[3])
     
+    # Calculate speed in wire coordinates
+    Uʷ = A * Uᵉ
+    
+    U1 = sqrt(max(Uʷ[1], 0.0))
+    U2 = sqrt(max(Uʷ[2], 0.0))
+    U3 = sqrt(max(Uʷ[3], 0.0))
+    
+    return (-U1*cosϕ[1,1] - U2*cosϕ[1,2] - U3*cosϕ[1,3],
+            -U1*cosϕ[2,1] - U2*cosϕ[2,2] - U3*cosϕ[2,3],
+            -U1*cosϕ[3,1] - U2*cosϕ[3,2] - U3*cosϕ[3,3])
+end
+
+function velocity!(anem::Probe3d, E::AbstractMatrix, T, idx=1:3)
+
+    npts = size(E,1)
+    for i in 1:npts
+        U, V, W = anem(E[i,idx[1]], E[i,idx[2]], E[i,idx[3]])
+        E[i,idx[1]], E[i,idx[2]], E[i,idx[3]] = U, V, W
+    end
+
 end
 
 
-
+function velocity(anem::Probe3d, E::AbstractMatrix, T, idx=1:3)
+    npts = size(E,1)
     
+    Uout = zeros(npts, 3)
+    
+    for i in 1:npts
+        U, V, W = anem(E[i,idx[1]], E[i,idx[2]], E[i,idx[3]])
+        Uout[i,idx[1]], Uout[i,idx[2]], Uout[i,idx[3]] = U, V, W
+    end
+
+    return Uout
+end
 
