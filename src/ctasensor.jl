b4a2ad73a0a2ce6d910397d982997e3113c2f7d2
= <pjabardo@ipt.br>
2023-05-26 16:04:10 -0300

    Started to work on CTASensor (ctasensor.jl)
    I think I found the correct structure for a flexible framework.
    
    The real issue is that up to now, CTASensor (and CCASensor) really didn't
    do anything! It just stored The operating resistance (and current for CCA).
    
    The idea now, is that CTASensor (and CCASensor) will handle most of the
    low level stuff, it will actually implement a proxy for a sensor!
    
    It now has besides the operating conditions (Rw for CTA and I for CCA),
    the calibration and correction procedures. The calibration will
    simply store the calibration curve and calibration conditions.
    
    The correction procedure will correct the readings due to changes in
    temperature, pressure and even fluid. It migh even take into account
    the heat transfer characteristics of the heating element - important when
    dealing with thermistors that hava an insulating layer and thick terminals
    where heat is dissipated.
    
    Now I need to get to work on this...

---
+++ src/ctasensor.jl
@@ -0,0 +1,22 @@
+
+
+struct CTASensor{T,U,RT,Calibr,Correct} <: AbstractCTA
+    R::RT
+    Rw::T
+    gain::U
+    cal::Calibr
+    corr::Correct
+end
+
+Base.broadcastable(sensor::CTASensor) = Ref(sensor)
+
+
+sensor(w::AbstractCTA) = w.R
+resistance(w::AbstractCTA) = w.Rw
+temperature(w::AbstractCTA) = temperature(sensor(w), resistance(w))
+overheatratio(w::AbstractCTA, T) = resistance(w) / resistance(sensor(w),T) - 1
+overheatratio(w::AbstractCTA) = resistance(w) / resistance(sensor(w)) - 1
+overtemp(w::AbstractCTA,T) = temperature(sensor(w),resistance(w)) - T
+overtemp(w::AbstractCTA) = temperature(sensor(w),resistance(w)) - temperature(sensor(w))
+
+
